834d5932eee0f6034a378ced89dc1fdd82984d66
Lewis O'Driscoll <lewis.odriscoll@gmail.com>
2018-10-30 13:57:24 +0000

    Remove unneeded gates, and calc logarithmic negativity of output state

--- qnn.py
+++ qnn.py
@@ -3,7 +3,9 @@ import numpy as np
 import strawberryfields as sf
 import matplotlib.pyplot as plt
 from qutip import Qobj
+from qutip.wigner import wigner
 from qutip.visualization import plot_wigner
+from scipy.integrate import trapz
 from strawberryfields.ops import *
 
 # Helper functions
@@ -16,14 +18,11 @@ def rand_complex(max_r):
     return r * np.exp(1j*theta)
 
 # Parameter values for the circuit
-thetas = np.array([rand_angle() for _ in range(4)])
 b_splitters = np.array([(rand_angle(), rand_angle()) for _ in range(2)])
 rs = np.array([rand_complex(1.4) for _ in range(2)])
 alphas = np.array([rand_complex(5.0) for _ in range(2)])
 alpha_in = 0.5 + 2j;
 
-print("Rotations:")
-print("    {}".format(thetas))
 print("Beam Splitters:")
 print("    {}".format(b_splitters))
 print("Squeezing:")
@@ -37,8 +36,6 @@ with eng:
     Coherent(alpha_in) | q[1]
 
     # Interferometer
-    Rgate(thetas[0]) | q[0]
-    Rgate(thetas[1]) | q[1]
     BSgate(*b_splitters[0]) | (q[0], q[1])
 
     # Squeezing
@@ -46,8 +43,6 @@ with eng:
     Sgate(rs[1]) | q[1]
 
     # Interferometer
-    Rgate(thetas[2]) | q[0]
-    Rgate(thetas[3]) | q[1]
     BSgate(*b_splitters[1]) | (q[0], q[1])
 
     # Displacement
@@ -66,10 +61,20 @@ print("Time to simulate: {}".format(dt))
 # Trace out the ancilla mode
 state_dm = state.reduced_dm(1)
 
+qstate = Qobj(state_dm)
 # Plot Wigner function of output state
-plot_wigner(Qobj(state_dm), colorbar=True)
+plot_wigner(qstate, colorbar=True)
 plt.show()
 
-# TODO: Use qutip.continuous_variables.covariance_matrix to calc cov mat of state
-# Also calc displacement vec, and plot wigner of corresponding Gaussian state for comparison
-# Calc fidelity between output and closest Gaussian state
+xvec = np.linspace(-10, 10, 500)
+W = wigner(qstate, xvec, xvec)
+# Integrate wigner function over all phase space to check for numerical errors
+int_W = trapz(W, xvec, xvec)
+int_W = trapz(int_W, xvec, xvec)
+print("Integral of Wigner fn: {}".format(int_W))
+
+# Integrate |W| over all of phase space and take log
+int_abs = trapz(np.abs(W), xvec, xvec)
+int_abs = trapz(int_abs, xvec, xvec)
+log_neg = np.log(int_abs)
+print("Logrithmic negativity: {}".format(log_neg))
