3cb05f3a98929d1669cc228ab75c409c81bd3c37
Lewis O'Driscoll <lewis.odriscoll@gmail.com>
2018-11-27 18:25:58 +0000

    Add multiple layers to QNN

--- qnn_tf.py
+++ qnn_tf.py
@@ -6,33 +6,44 @@ from states import cubic_phase
 
 # Truncation to use in simulation
 trunc = 25
+# Number of layers in QNN
+n_layers = 2
 
 # Parameters for the circuit
-b_splitters = tf.Variable(initial_value=tf.random_uniform([2, 2], maxval=2*np.pi),
+b_splitters = tf.Variable(initial_value=tf.random_uniform([n_layers,2], maxval=2*np.pi),
     dtype=tf.float32, constraint=lambda x: tf.clip_by_value(x, 0, 2*np.pi))
-rs = tf.Variable(initial_value=tf.random_uniform([2], minval=-1.4, maxval=1.4),
+rs = tf.Variable(initial_value=tf.random_uniform([n_layers, 2], minval=-1.4, maxval=1.4),
     dtype=tf.float32, constraint=lambda x: tf.clip_by_value(x, -1.4, 1.4))
-alphas = tf.Variable(initial_value=tf.random_normal([2], mean=1, stddev=4),
+alphas = tf.Variable(initial_value=tf.random_normal([n_layers, 2], mean=1, stddev=4),
     dtype=tf.float32)
+
+def layer(q, n):
+    """
+    Implements one layer of a QNN
+    :param q: The strawberryfields quantum register
+    :param n: The layer number
+    """
+    # Interferometer
+    BSgate(b_splitters[n][0], -np.pi/4) | (q[0], q[1])
+
+    # Squeezing
+    Sgate(rs[n][0]) | q[0]
+    Sgate(rs[n][1]) | q[1]
+
+    # Interferometer
+    BSgate(b_splitters[n][1], -np.pi/4) | (q[0], q[1])
+
+    # Displacement
+    Dgate(alphas[n][0]) | q[0]
+    Dgate(alphas[n][1]) | q[1]
 
 eng, q = sf.Engine(2)
 with eng:
     Fock(1) | q[0]
     Vac | q[1]
 
-    # Interferometer
-    BSgate(b_splitters[0][0], b_splitters[0][1]) | (q[0], q[1])
-
-    # Squeezing
-    Sgate(rs[0]) | q[0]
-    Sgate(rs[1]) | q[1]
-
-    # Interferometer
-    BSgate(b_splitters[1][0], b_splitters[1][0]) | (q[0], q[1])
-
-    # Displacement
-    Dgate(alphas[0]) | q[0]
-    Dgate(alphas[1]) | q[1]
+    for n in range(n_layers):
+        layer(q, n)
 
     # Measurement
     MeasureFock(select=0) | q[0]
@@ -47,8 +58,8 @@ target_state = cubic_phase(trunc, 0.005, -1.0).full()
 target_dm = target_state @ target_state.conj().T
 
 # Calculate the fidelity of the output state with the cubic phase state
-fid = tf.trace(state_dm @ target_dm)
-loss = -tf.abs(fid)
+fid = tf.abs(tf.trace(state_dm @ target_dm)) # Output of tf.trace should be real, but usually has very small imaginary component
+loss = -fid
 
 # Tell tensorflow what to optimise
 optimiser = tf.train.GradientDescentOptimizer(learning_rate=0.01)
